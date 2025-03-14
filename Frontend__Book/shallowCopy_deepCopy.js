af084fa2309a6b9caa1552bf28c62d8b6a5ef4b5
hithesh27 <hitheshpk.27@gmail.com>
2025-03-14 19:24:49 +0530

    changed

--- Frontend__Book/shallowCopy_deepCopy.js
+++ Frontend__Book/shallowCopy_deepCopy.js
@@ -1,26 +1,3 @@
 /* 
-Saying an object is duplicated does not confirm whether it is shallow or deep copy.
-*/
-
-/* 
-    const person={
-        name : "Hithesh",
-        address :{
-            street : "Srinagar colony",
-            pincode : 518301
-        }
-    }
-    person is the top level object.address is nested object.
-*/
-
-//Shallow copy :
-/* 
-It is  only the top level object is new.I mean reference is new for whole objects.
-That means  proties are copied but nested elements are referenced.
-NOte : spread operator always create a shallow copy.
-*/
-
-//deep copy :
-/* 
-Everything is copied even the nested elements.
+PLS refer shallowCopy__deepCopy.js
 */
